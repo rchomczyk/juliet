7f9c8c80c27ae96e8f33cfc7d85a7a43029200b5
Rafał Chomczyk <rchomczyk@icloud.com>
2023-08-08 12:53:36 +0200

    Initial proof of concept of juliet
    * Add written proof of concept for juliet
    
    * Add tests covering HikariConfigBuilder
    
    * Add tests covering HikariPooledDatasource along with the possibility to specify custom driver class name in HikariConfigBuilder
    
    * Get rid of mockito, as it is not used anywhere at this moment
    
    * Use proper driver class name to fix pipeline run
    
    * Load driver class name into classpath during pipeline run
    
    * Fix logging-dependency conflict
    
    * Fix pipeline run caused by invalid driver class name
    
    * Add tests covering HikariPooledDatasource#close
    
    * Add tests covering JulietBuilder
    
    * Add tests covering JulietImpl

---
+++ src/main/java/moe/rafal/juliet/datasource/HikariConfigBuilder.java
@@ -0,0 +1,83 @@
+/*
+ *    Copyright 2023 juliet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package moe.rafal.juliet.datasource;
+
+import com.zaxxer.hikari.HikariConfig;
+
+public final class HikariConfigBuilder {
+
+  private String driverClassName;
+  private String jdbcUri;
+  private String username;
+  private String password;
+
+  private HikariConfigBuilder() {
+
+  }
+
+  public static HikariConfigBuilder newBuilder() {
+    return new HikariConfigBuilder();
+  }
+
+  public HikariConfigBuilder withDriverClassName(String driverClassName) {
+    this.driverClassName = driverClassName;
+    return this;
+  }
+
+  public HikariConfigBuilder withJdbcUri(String jdbcUri) {
+    this.jdbcUri = jdbcUri;
+    return this;
+  }
+
+  public HikariConfigBuilder withUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public HikariConfigBuilder withPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public HikariConfig build() throws HikariConfigBuildException {
+    if (jdbcUri == null) {
+      throw new HikariConfigBuildException(
+          "Hikari config could not be built, without specifying jdbc uri, which is required.");
+    }
+
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setJdbcUrl(jdbcUri);
+
+    boolean whetherCustomDriverIsSpecified = driverClassName != null;
+    if (whetherCustomDriverIsSpecified) {
+      hikariConfig.setDriverClassName(driverClassName);
+    }
+
+    final boolean whetherUsernameIsSpecified = username != null;
+    if (whetherUsernameIsSpecified) {
+      hikariConfig.setUsername(username);
+    }
+
+    final boolean whetherPasswordIsSpecified = password != null;
+    if (whetherPasswordIsSpecified) {
+      hikariConfig.setPassword(password);
+    }
+
+    return hikariConfig;
+  }
+}
---
+++ src/test/java/moe/rafal/juliet/JulietImplTests.java
@@ -0,0 +1,61 @@
+/*
+ *    Copyright 2023 juliet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package moe.rafal.juliet;
+
+import static moe.rafal.juliet.JulietUtils.produceJulietByContainer;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.testcontainers.utility.DockerImageName.parse;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+class JulietImplTests {
+
+  @Container
+  private final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(parse("mysql:latest"))
+      .withReuse(true);
+
+  @Test
+  void verifyWhetherContainerIsRunningTest() {
+    assertThat(mySQLContainer.isRunning())
+        .isTrue();
+  }
+
+  @Test
+  void verifyWhetherConnectionWasResolvedSuccessfullyTest() throws SQLException {
+    try (Connection connection = produceJulietByContainer(mySQLContainer).borrowConnection()) {
+      assertThat(connection).isNotNull();
+    }
+  }
+
+  @Test
+  void verifyWhetherConnectionIsBeingTerminatedTest() throws SQLException {
+    Juliet Juliet = produceJulietByContainer(mySQLContainer);
+    Connection connection = Juliet.borrowConnection();
+    Juliet.close();
+    assertThatCode(connection::createStatement)
+        .isInstanceOf(SQLException.class)
+        .hasMessage("No operations allowed after connection closed.");
+  }
+}
